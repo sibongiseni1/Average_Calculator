fbb2211b291e0706f82bb2d84404655af00be226
Sibongiseni Mthetwa <137326611+sibongiseni1@users.noreply.github.com>
2023-08-02 19:59:03 +0200

    Update Average_Calculator.cpp
    Average Calculation using C++

--- Average_Calculator.cpp
+++ Average_Calculator.cpp
@@ -10,18 +10,11 @@ int main()
     int counter = 0;
 
     for(int k =0; k < 5; k++){
-        cout << "Enter the student's mark " << (k + 1) << ": ";
+        cout << "Enter the values " << (k + 1) << ": ";
         cin >> mark;
-
-        if(mark >= 75){
-            counter++;
-        }
         sum += mark;
     }
-
     average = sum / 5.0;
     cout << "The average is: " << average << endl;
-    cout << "A total of " << counter << " distinctions!" << endl;
-
     return 0;
 }
